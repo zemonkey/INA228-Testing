597c95e0f9e228289bfbe7cf6c4b7020aa7c1992
zemonkey <61571621+zemonkey@users.noreply.github.com>
2021-07-31 07:27:35 +0700

    Update INA228.ino

--- INA228/INA228.ino
+++ INA228/INA228.ino
@@ -6,7 +6,7 @@
 #include <Wire.h>
 
 int reading = 0;
-
+int ADC_Val = 0; //int on ESP32 is 32-bit or 4 bytes
  
 void setup() {
   Wire.begin();
@@ -15,6 +15,7 @@ void setup() {
 }
  
 void loop() {
+  //baca device ID
   Wire.beginTransmission(64);
   Wire.write(byte(0x01));      // sets register pointer to the command register (0x00)
   Wire.endTransmission();      // stop transmitting
@@ -32,6 +33,54 @@ void loop() {
 
     Serial.println(reading); 
 
-delay(1000);
+  
 }
+  delay(1000);
+
+
+  //baca VShunt
+  Wire.beginTransmission(64);
+  Wire.write(byte(0x04));      // sets register pointer to the command register (0x00)
+  Wire.endTransmission();      // stop transmitting
+  Wire.requestFrom(64, 3);    // request 2 bytes from slave device #112
+
+  // step 5: receive reading from sensor
+
+  if (3 <= Wire.available()) { // if two bytes were received
+
+    reading = Wire.read();  // receive high byte (overwrites previous reading)
+
+    reading = reading << 8;    // shift high byte to be high 8 bits
+
+    reading |= Wire.read(); // receive low byte as lower 8 bits
+
+    Serial.println(reading); 
+
+  
+}
+  delay(1000);
+
+
+    //baca VBus
+  Wire.beginTransmission(64);
+  Wire.write(byte(0x05));      // sets register pointer to the command register (0x00)
+  Wire.endTransmission();      // stop transmitting
+  Wire.requestFrom(64, 3);    // request 2 bytes from slave device #112
+
+  // step 5: receive reading from sensor
+
+  if (3 <= Wire.available()) { // if two bytes were received
+
+    reading = Wire.read();  // receive high byte (overwrites previous reading)
+
+    reading = reading << 8;    // shift high byte to be high 8 bits
+
+    reading |= Wire.read(); // receive low byte as lower 8 bits
+
+    Serial.println(reading); 
+
+  
+}
+  delay(1000);
+  Serial.println("End of File");
 }
